ae7d882fec4fbfb0ae54bbd50f0ebccd419cd35f
Franco Cruzat <fhcruzat@miuandes.cl>
2020-03-24 15:01:33 -0300

    pregunta 1 completa

---
+++ laboratorio1_francocruzat/laboratorio1_francocruzat/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace laboratorio1_francocruzat
+{   
+    class Person
+    {
+        string Nombre;
+        string Apellido;
+        public int Lanzar()
+        {
+            var rand = new Random();
+            int numero = rand.Next(0, 3);
+            return numero;
+        }
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var persona = new Person();
+            int lanzada = persona.Lanzar();
+            Console.WriteLine(lanzada);
+        }
+    }
+}
